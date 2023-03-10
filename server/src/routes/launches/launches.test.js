f99855b843609b097ff81281ef4cc87654628374
Ana-teston <anapaulateston@gmail.com>
2023-03-10 12:10:22 +0100

    solving issues in the jest libraries and mongoose connect

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,69 +1,88 @@
 const request = require('supertest');
 const app = require('../../app');
-describe('Test GET/launches', () => {
+const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
-
-    test('It should respond whit 200 success', async () => {
-        const response = await request(app)
-            .get('/launches')
-            .expect('Content-Type', /json/)
-            .expect(200);
+describe('Launches API', () => {
+    beforeAll(async () => {
+        await mongoConnect();
+        await loadPlanetsData();
     });
-});
 
-describe('Test POST/launch', () => {
-
-    const completeLaunchData = {
-        mission: 'USS Enterprise',
-        rocket: 'NCC 1701-D',
-        target: 'Kepler-186 f',
-        launchDate: 'january 4, 2028',
-    };
-
-    const launchDataWithoutDate = {
-        mission: 'USS Enterprise',
-        rocket: 'NCC 1701-D',
-        target: 'Kepler-186 f',
-    }
-
-    const launchDataWithInvalidDate = {
-        mission: 'USS Enterprise',
-        rocket: 'NCC 1701-D',
-        target: 'Kepler-186 f',
-        launchDate: 'zoot',
-    }
-
-    test('It should respond with 201 created', async () => {
-        const response = await request(app)
-            .post('/launches')
-            .send(completeLaunchData)
-            .expect('Content-Type', /json/)
-            .expect(201);
-        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
-        const responseDate = new Date(response.body.launchDate).valueOf();
-        expect(responseDate).toBe(requestDate);
-        expect(response.body).toMatchObject(launchDataWithoutDate)
+    afterAll(async () => {
+        await mongoDisconnect();
     });
-    test('It should catch missing require properties', async () => {
-        const response = await request(app)
-            .post('/launches')
-            .send(launchDataWithoutDate)
-            .expect('Content-Type', /json/)
-            .expect(400);
-        expect(response.body).toStrictEqual({
-            error: 'Missing required launch property'
-        })
+
+    describe('Test GET /launches', () => {
+        jest.setTimeout(30000);
+        test('It should respond with 200 success', async () => {
+            const response = await request(app)
+                .get('/launches')
+                .expect('Content-Type', /json/)
+                .expect(200);
+        });
     });
-    test('It should catch invalid dates', async () => {
-        const response = await request(app)
-            .post('/launches')
-            .send(launchDataWithInvalidDate)
-            .expect('Content-Type', /json/)
-            .expect(400);
-        expect(response.body).toStrictEqual({
-            error: 'Invalid launch date'
+
+    describe('Test POST /launch', () => {
+        // set a longer timeout for this test
+        jest.setTimeout(30000);
+
+        const completeLaunchData = {
+            mission: 'USS Enterprise',
+            rocket: 'NCC 1701-D',
+            target: 'Kepler-62 f',
+            launchDate: 'January 4, 2028',
+        };
+
+        const launchDataWithoutDate = {
+            mission: 'USS Enterprise',
+            rocket: 'NCC 1701-D',
+            target: 'Kepler-62 f',
+        };
+
+        const launchDataWithInvalidDate = {
+            mission: 'USS Enterprise',
+            rocket: 'NCC 1701-D',
+            target: 'Kepler-62 f',
+            launchDate: 'zoot',
+        };
+
+        test('It should respond with 201 created', async () => {
+            const response = await request(app)
+                .post('/launches')
+                .send(completeLaunchData)
+                .expect('Content-Type', /json/)
+                .expect(201);
+
+            const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+            const responseDate = new Date(response.body.launchDate).valueOf();
+            expect(responseDate).toBe(requestDate);
+
+            expect(response.body).toMatchObject(launchDataWithoutDate);
         });
 
-    });
+        test('It should catch missing required properties', async () => {
+            const response = await request(app)
+                .post('/launches')
+                .send(launchDataWithoutDate)
+                .expect('Content-Type', /json/)
+                .expect(400);
 
+            expect(response.body).toStrictEqual({
+                error: 'Missing required launch property',
+            });
+        });
+
+        test('It should catch invalid dates', async () => {
+            const response = await request(app)
+                .post('/launches')
+                .send(launchDataWithInvalidDate)
+                .expect('Content-Type', /json/)
+                .expect(400);
+
+            expect(response.body).toStrictEqual({
+                error: 'Invalid launch date',
+            });
+        });
+    });
 });
--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,18 @@
 const http = require('http');
 
-const mongoose = require('mongoose');
-
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
-
-const MONGO_URL = 'mongodb+srv://nasa-api:password.mongodb.net/nasa?retryWrites=true&w=majority';
+const {mongoConnect} = require("./services/mongo");
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
-mongoose.connection.once('open', () => {
-    console.log('MongoDb connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-});
 
 async function startServer () {
-    await mongoose.connect(MONGO_URL);
+    await mongoConnect();
     await loadPlanetsData();
+
     server.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`);
+        console.log(`Listening on port ${PORT}...`);
     });
 }
 startServer()
